2cf40572377299fcb448e29be18b72c4133af8c4
pankajmit <pankajmit@hotmail.com>
2015-12-21 11:14:27 -0800

    Final code after comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## This functions will cache the mean of the Matrix and keep it in the memory
-## functions do
+## This file is created by pankajmit as part of coursera programming assignment 2
 
-## Write a short comment describing this function - updated by Pankaj
+
+## This funcion creates a special vector for creating a list containing a function to 
+## 1. Set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the Inverted Matrix
+## 4. get the value of the Inverted Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -13,13 +17,15 @@ makeCacheMatrix <- function(x = matrix()) {
   getMatrix <- function() x
   setInverseMatrix <- function(InputMatrix) InverseMatrix <<- InputMatrix
   getInverseMatrix <- function() InverseMatrix
-  list(setMAtrix = setMatrix, getMatrix = getMatrix,
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
        setInverseMatrix = setInverseMatrix,
        getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## The following function returns the inverted matrix of the special "matrix" created with the above function
+## It first checks to see if the inverted matrix is already available and cached. If yes, cached inverted matrix is returned.
+## If no, inverse matrix is first calculated, saves it in cache and then return the inverted matrix 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -29,7 +35,7 @@ cacheSolve <- function(x, ...) {
     message("getting cached data")
     return(InvertedMatrix)
   }
-  data <- x$getInverseMatrix()
+  data <- x$getMatrix()
   InvertedMatrix <- solve(data)
   x$setInverseMatrix(InvertedMatrix)
   InvertedMatrix
